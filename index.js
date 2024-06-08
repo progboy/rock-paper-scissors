a0376d84f41d5aff0da60e598f744f9af253b40e
progboy <manzarahmad77@gmail.com>
2024-06-08 10:52:16 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,45 @@
+const buttons = document.querySelectorAll("button");
+const options = ["Rock", "Paper", "Scissors"];
+const playerChoice = document.getElementById("playerchoice");
+const compChoice = document.getElementById("compchoice");
+const result = document.getElementById("result");
+const playerscore = document.getElementById("playerscore");
+const compscore = document.getElementById("compscore");
+
+
+// to-do
+// define draw,win,lose functions
+// fix outside border of container
+
+function draw(){
+    result.textContent = "IT'S A TIE";
+    result.style.color = "black";
+}
+function win(){
+    result.textContent = "YOU WIN!!!";
+    result.style.color = "green";
+    playerscore.textContent = Number(playerscore.textContent)+1;
+
+}
+function lose(){
+    result.textContent = "you lost";
+    result.style.color = "red";
+    compscore.textContent = Number(compscore.textContent)+1;
+}
+function playRound(event){
+    let pChoice = event.target.id;
+    let cChoice = options[Math.floor(3*Math.random())];
+
+    playerChoice.innerHTML = pChoice;
+    compChoice.innerHTML = cChoice;
+    
+    if(pChoice===cChoice){
+        draw();
+    }else if(pChoice === "Rock" && cChoice === "Scissors" || pChoice === "Paper" && cChoice === "Rock" ||
+         pChoice === "Scissors" && cChoice === "Paper"){
+            win();
+    }else{
+        lose();
+    }
+}
+buttons.forEach(button => button.addEventListener("click", playRound));
